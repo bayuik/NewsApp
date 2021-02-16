739e0263fce1d964efe2a5b3d4a0cc0734d4840d
bayuik <bayuindrakusuma05.com>
2021-02-16 11:30:56 +0700

    list articles

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/article.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp exteds Statelesswidget {
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'News App',
       theme: ThemeData(
@@ -23,16 +24,37 @@ class NewsListPage extends StatelessWidget {
   static const routeName = '/article_list';
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('News App'),
       ),
       body: FutureBuilder<String>(
-        future: 
-            DefaultAssetBUndle.of(context).loadString('assets/articles.json'),
-        builder: (context, snapshot){},
+        future:
+            DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+        builder: (context, snapshot) {
+          final List<Article> articles = parseArticles(snapshot.data);
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return _buildArticleItem(context, articles[index]);
+            },
+          );
+        },
       ),
+    );
+  }
+
+  Widget _buildArticleItem(BuildContext context, Article article) {
+    return ListTile(
+      contentPadding:
+          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      leading: Image.network(
+        article.urlToImage,
+        width: 100,
+      ),
+      title: Text(article.title),
+      subtitle: Text(article.author),
     );
   }
 }
